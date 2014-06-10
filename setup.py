ff1b60e55697db33009c0774f7e081300ee3743f
Harald Friessnegger <harald@webmeisterei.com>
2014-06-10 11:25:19 +0200

    prepare release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 import sys
 from setuptools import setup, find_packages
 
-version = '0.4.6dev'
+version = '0.4.6'
 
 if sys.version_info[0] == 2 and sys.version_info[1] < 6:
     requires = ['simplejson']
