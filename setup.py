3d17ed846d218ce3cca832c091f7e2a3ea8b526f
Simon Delcourt <delcourt.simon@gmail.com>
2015-08-06 09:17:59 +0200

    Back to development: 0.4.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 import sys
 from setuptools import setup, find_packages
 
-version = '0.4.7'
+version = '0.4.8.dev0'
 
 if sys.version_info[0] == 2 and sys.version_info[1] < 6:
     requires = ['simplejson']
